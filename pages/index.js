e1dafbf5558b29f5f6b5307970d294551cbbac77
Arthur Fernandes <118545996+Arthur-Fernandes-Barros@users.noreply.github.com>
2026-01-07 19:06:10 +0000

    Atualização index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>Te amo meu amor!!</h1>
-      <img
-        src="https://i.pinimg.com/originals/7d/8b/1d/7d8b1d84e2471ad2b5b1c3e9e1181e58.gif"
-        alt="Coração"
-      />
+      <h1>Kayuri te amo meu amor</h1>
     </div>
   );
 }
