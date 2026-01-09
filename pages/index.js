d9e395a4d2ce780b3775087579e95f780d8d48a4
Arthur Fernandes <118545996+Arthur-Fernandes-Barros@users.noreply.github.com>
2026-01-09 01:27:31 +0000

    Adição Gif na mensagem

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ function Home() {
   return (
     <div>
       <h1>Kayuri te amo meu amor</h1>
+      <img src ="https://media.tenor.com/pHcjNsn7hNYAAAAi/heart.gif" alt="Heart Animation" />
     </div>
   );
 }
